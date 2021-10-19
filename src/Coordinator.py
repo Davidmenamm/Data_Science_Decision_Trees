f26fb1749dbffa5c29315c419e1a4a987ad2d7d8
David Mena <dmena@libelulasoft.com>
2021-10-18 19:54:24 -0500

    ADD: initial program structure

---
+++ src/Coordinator.py
@@ -0,0 +1,16 @@
+""" Manage All the Program """
+
+# imports
+
+
+# input paths
+mainInputPath = r'data\input'
+
+# output paths
+mainOutputPath = r'data\output'
+# path
+
+
+# Coordinator
+def coordinate():
+    a
